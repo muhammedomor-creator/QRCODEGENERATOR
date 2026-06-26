051744efa39147cebaf2ecb77de4f922327bc14b
Muhammed Omor <muhammedomor.youtube@gmail.com>
2026-06-26 15:08:49 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+
+const CACHE_NAME = 'scanflow-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event (Network First Fallback to Cache)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return caches.match(e.request);
+    })
+  );
+});
